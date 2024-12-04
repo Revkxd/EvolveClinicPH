1f19eff9895678053d5f3ac87896bc00ddba6c1e
Revkxd <79917173+Revkxd@users.noreply.github.com>
2024-12-05 03:20:23 +0800

    Add vercel speed insights

--- evolveclinicph/src/app/layout.tsx
+++ evolveclinicph/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} custom-scrollbar`}>
+        <SpeedInsights />
         <Navbar />
         {children}
         <Footer />
