b7e50b781269280d14f29f4d09d49e11374ce8be
Revkxd <79917173+Revkxd@users.noreply.github.com>
2024-12-05 01:43:17 +0800

    modified Footer

--- evolveclinicph/src/app/components/Footer.tsx
+++ evolveclinicph/src/app/components/Footer.tsx
@@ -1,11 +1,9 @@
-const Footer = () => {
+export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-8">
+    <footer className="bg-turq-shaded2 text-white py-8">
       <div className="container mx-auto px-4 text-center">
-        <p>&copy; 2023 Evolve Clinic PH. All rights reserved.</p>
+        <p>&copy; 2024 Evolve Clinic PH. All rights reserved.</p>
       </div>
     </footer>
   )
 }
-
-export default Footer
