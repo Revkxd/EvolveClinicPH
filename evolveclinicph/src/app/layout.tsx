cd290a526e5ef0c089845338bc77b216cb45cf41
Revkxd <79917173+Revkxd@users.noreply.github.com>
2024-12-07 00:55:32 +0800

    BUGFIX: Navbar covering page content

--- evolveclinicph/src/app/layout.tsx
+++ evolveclinicph/src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <SpeedInsights />
         <div className='flex flex-col min-h-screen'>
           <Navbar />
-          <main className='flex-grow'>
+          <main className='flex-grow pt-24 md:pt-32'>
             {children}
           </main>
           <Footer />
