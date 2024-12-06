52cafa6b7a93998e4698e372ec189fe29ca3aefb
Revkxd <79917173+Revkxd@users.noreply.github.com>
2024-12-07 00:46:31 +0800

    Improved navbar behavior

--- evolveclinicph/src/app/components/Navbar.tsx
+++ evolveclinicph/src/app/components/Navbar.tsx
@@ -1,18 +1,34 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
+  const [prevScrollPos, setPrevScrollPos] = useState(0)
+  const [visible, setVisible] = useState(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset
+      const isScrollingUp = prevScrollPos > currentScrollPos
+      const isAtTop = currentScrollPos < 10
+
+      setVisible(isScrollingUp || isAtTop)
+      setPrevScrollPos(currentScrollPos)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [prevScrollPos])
 
   function closeMenu() {
-    setIsOpen(false);
+    setIsOpen(false)
   }
 
   return (
-    <nav className="bg-white shadow-lg">
+    <nav className={`bg-white shadow-lg fixed w-full z-10 transition-transform duration-300 ${visible ? 'translate-y-0' : '-translate-y-full'}`}>
       <div className="max-w-6xl mx-auto px-4">
         <div className="flex justify-between">
           <div className="flex space-x-7">
@@ -39,10 +55,10 @@ export default function Navbar() {
         </div>
       </div>
       <div className={`${isOpen ? 'block' : 'hidden'} md:hidden`}>
-        <Link href="services" className="block py-2 px-5 text-xl hover:bg-turq hover:text-white transition duration-300" onClick={closeMenu}>Services</Link>
-        <Link href="bookings" className="block py-2 px-5 text-xl hover:bg-turq hover:text-white transition duration-300" onClick={closeMenu}>Bookings</Link>
-        <Link href="about" className="block py-2 px-5 text-xl hover:bg-turq hover:text-white transition duration-300" onClick={closeMenu}>About Us</Link>
-        <Link href="contact" className="block py-2 px-5 text-xl hover:bg-turq hover:text-white transition duration-300" onClick={closeMenu}>Contact Us</Link>
+        <Link href="/services" className="block py-2 px-5 text-xl hover:bg-turq hover:text-white transition duration-300" onClick={closeMenu}>Services</Link>
+        <Link href="/bookings" className="block py-2 px-5 text-xl hover:bg-turq hover:text-white transition duration-300" onClick={closeMenu}>Bookings</Link>
+        <Link href="/about" className="block py-2 px-5 text-xl hover:bg-turq hover:text-white transition duration-300" onClick={closeMenu}>About Us</Link>
+        <Link href="/contact" className="block py-2 px-5 text-xl hover:bg-turq hover:text-white transition duration-300" onClick={closeMenu}>Contact Us</Link>
       </div>
     </nav>
   )
