cf48938c12f8b61e72296700bdaab0c3649996c4
Revkxd <79917173+Revkxd@users.noreply.github.com>
2024-12-09 11:35:30 +0800

    Flipped theme of navbar

--- evolveclinicph/src/app/components/Navbar.tsx
+++ evolveclinicph/src/app/components/Navbar.tsx
@@ -28,25 +28,25 @@ export default function Navbar() {
   }
 
   return (
-    <nav className={`bg-white shadow-lg fixed w-full z-10 transition-transform duration-300 ${visible ? 'translate-y-0' : '-translate-y-full'}`}>
+    <nav className={`bg-turq-shaded shadow-lg fixed w-full z-10 transition-transform duration-300 ${visible ? 'translate-y-0' : '-translate-y-full'}`}>
       <div className="max-w-6xl mx-auto px-4">
         <div className="flex justify-between">
           <div className="flex space-x-7">
             <div>
               <Link href="/" className="flex items-center py-4 px-2">
-                <Image src="/logo-dark.png" alt="Evolve Clinic PH Logo" width={120} height={120} />
+                <Image src="/logo-light.png" alt="Evolve Clinic PH Logo" width={120} height={120} />
               </Link>
             </div>
           </div>
           <div className="hidden md:flex items-center space-x-1">
-            <Link href="/services" className="py-4 px-4 text-black text-3xl font-semibold hover:text-turq transition duration-300">Services</Link>
-            <Link href="/bookings" className="py-4 px-4 text-black text-3xl font-semibold hover:text-turq transition duration-300">Bookings</Link>
-            <Link href="/about" className="py-4 px-4 text-black text-3xl font-semibold hover:text-turq transition duration-300">About Us</Link>
-            <Link href="/contact" className="py-4 px-4 text-black text-3xl font-semibold hover:text-turq transition duration-300">Contact Us</Link>
+            <Link href="/services" className="py-4 px-4 text-white text-3xl font-semibold hover:text-gray-300 transition duration-300">Services</Link>
+            <Link href="/bookings" className="py-4 px-4 text-white text-3xl font-semibold hover:text-gray-300 transition duration-300">Bookings</Link>
+            <Link href="/about" className="py-4 px-4 text-white text-3xl font-semibold hover:text-gray-300 transition duration-300">About Us</Link>
+            <Link href="/contact" className="py-4 px-4 text-white text-3xl font-semibold hover:text-gray-300 transition duration-300">Contact Us</Link>
           </div>
           <div className="md:hidden flex items-center">
             <button className="outline-none mobile-menu-button" onClick={() => setIsOpen(!isOpen)}>
-              <svg className="w-10 h-10 text-black hover:text-turq"
+              <svg className="w-10 h-10 text-white hover:text-gray-300"
                 fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
                 <path d="M4 6h16M4 12h16M4 18h16"></path>
               </svg>
@@ -55,10 +55,10 @@ export default function Navbar() {
         </div>
       </div>
       <div className={`${isOpen ? 'block' : 'hidden'} md:hidden`}>
-        <Link href="/services" className="block py-2 px-5 text-xl hover:bg-turq hover:text-white transition duration-300" onClick={closeMenu}>Services</Link>
-        <Link href="/bookings" className="block py-2 px-5 text-xl hover:bg-turq hover:text-white transition duration-300" onClick={closeMenu}>Bookings</Link>
-        <Link href="/about" className="block py-2 px-5 text-xl hover:bg-turq hover:text-white transition duration-300" onClick={closeMenu}>About Us</Link>
-        <Link href="/contact" className="block py-2 px-5 text-xl hover:bg-turq hover:text-white transition duration-300" onClick={closeMenu}>Contact Us</Link>
+        <Link href="/services" className="block py-2 px-5 text-xl text-white hover:text-gray-300 transition duration-300" onClick={closeMenu}>Services</Link>
+        <Link href="/bookings" className="block py-2 px-5 text-xl text-white hover:text-gray-300 transition duration-300" onClick={closeMenu}>Bookings</Link>
+        <Link href="/about" className="block py-2 px-5 text-xl text-white hover:text-gray-300 transition duration-300" onClick={closeMenu}>About Us</Link>
+        <Link href="/contact" className="block py-2 px-5 text-xl text-white hover:text-gray-300 transition duration-300" onClick={closeMenu}>Contact Us</Link>
       </div>
     </nav>
   )
