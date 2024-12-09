f5f6428299f3f254edc97109c8a3c6a985652920
Revkxd <79917173+Revkxd@users.noreply.github.com>
2024-12-09 21:43:26 +0800

    Added more metadata

--- evolveclinicph/src/app/layout.tsx
+++ evolveclinicph/src/app/layout.tsx
@@ -10,7 +10,23 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Evolve Clinic PH',
-  description: 'Your premier destination for advanced skincare treatments and cosmetic surgical procedures',
+  description: 'Discover advanced skincare treatments and professional cosmetic surgery at Evolve Clinic PH, your trusted aesthetic clinic in Quezon City.',
+  keywords: [
+    'Evolve Clinic PH', 
+    'skincare clinic White Plains', 
+    'skincare Quezon City', 
+    'cosmetic surgery Quezon City', 
+    'advanced skincare treatments', 
+    'cosmetic surgery', 
+    'laser treatments', 
+    'aesthetic procedures', 
+    'laser therapy', 
+    'anti-aging treatments', 
+    'dermatology',
+    'skin rejuvenation',
+  ],
+  robots: 'index, follow',
+  viewport: 'width=device-width, initial-scale=1.0',
 }
 
 export default function RootLayout({
